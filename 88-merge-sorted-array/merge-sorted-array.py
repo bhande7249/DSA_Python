c1bdc2f2bae8d99b7c1ebcae945fb66e9888334f
Rohan Bhande <148352207+bhande7249@users.noreply.github.com>
2026-08-14 21:20:51 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (92.13%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -7,20 +7,6 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-
-        # if m==1:
-        #     return;
-        # elif n==1:
-        #     nums1[0]=nums2[0];
-        #     return;
-        # j=m-1
-        # for i in range(0,len(nums1)):
-        # # for num in nums1:
-        #     # if nums1[i]==0:
-        #         nums1[j]=nums2[j]
-        #         j+=1;
-        # nums1.sort();
-
         i=m;
         for num in nums2:
             nums1[i]=num;
