a952a701ce9c105d5f623825876a763be429a0ec
Rohan Bhande <148352207+bhande7249@users.noreply.github.com>
2026-08-15 23:22:15 +0530

    Time: 8 ms (41.02%) | Memory: 13.6 MB (19.83%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,12 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        for i in range(0,len(nums)):
-            if nums[i]==0:
-                j=i+1;
-                while j<len(nums):
-                    if nums[j]!=0:
-                        nums[i],nums[j]=nums[j],nums[i]
-                        break;
-                    j+=1;
+
+        # timecomplecity is O(n^2)
+        # for i in range(0,len(nums)):
+        #     if nums[i]==0:
+        #         j=i+1;
+        #         while j<len(nums):
+        #             if nums[j]!=0:
+        #                 nums[i],nums[j]=nums[j],nums[i]
+        #                 break;
+        #             j+=1; 
             
+        # optimal solution
+        inser_pos=0;
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[inser_pos],nums[i]=nums[i],nums[inser_pos];
+                inser_pos+=1;
